dea53db4cdcd0fa027f44f32993361cff6542378
renodubois <rtdm9c@mst.edu>
2015-12-24 23:48:08 -0600

    Added encryption to passwords.
    Trying this out, but since my Firefox doesn’t like opening PHP files,
    having to commit and send to the web server.

--- login.php
+++ login.php
@@ -87,7 +87,7 @@
   if(count($_POST) > 0) {
     $conn = mysql_connect("localhost","root","");
     mysql_select_db("users",$conn);
-    $result = mysql_query("SELECT * FROM details WHERE userName='" . $_POST["userName"] . "' and password = '". $_POST["password"]."'");
+    $result = mysql_query("SELECT * FROM details WHERE userName='" . $_POST["userName"] . "' and password = SHA1('". $_POST["password"]."')");
     $count = mysql_num_rows($result);
     if($count == 0) {
       $message = "Invalid Username or Password!";
