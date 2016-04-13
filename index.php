e7ca2d214772f457429f708e600c66deb015fca5
Reno DuBois <rdubois96@gmail.com>
2016-04-13 14:29:08 -0500

    Renamed index.html to index.php

--- index.php
+++ index.php
@@ -33,20 +33,19 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.html">Rolla Smash Bros. Community</a>
+          <a class="navbar-brand" href="index.php">Rolla Smash Bros. Community</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="index.html">Home<span class="sr-only">(current)</span></a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Rankings</a></li>
             <li><a href="forum.html">Forums</a></li>
           </ul>
           <div class="text-uppercase">
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="#">Sign Up</a></li>
+              <li><a href="signup.php">Sign Up</a></li>
               <li><a href="login.php"><span class="glyphicon glyphicon-user"></span> Log In</a></li>
             </ul>
           </div>
